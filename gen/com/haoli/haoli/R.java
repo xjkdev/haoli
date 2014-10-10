5316a7cc4a40c97043348724a1460cf9e6a9e801
xjkdev <xjk2008@gmail.com>
2014-10-10 17:23:50 +0800

    add new add_price_dialog

--- gen/com/haoli/haoli/R.java
+++ gen/com/haoli/haoli/R.java
@@ -28,33 +28,51 @@ public final class R {
         public static final int ic_menu_done_holo_light=0x7f020003;
     }
     public static final class id {
-        public static final int action_add=0x7f080012;
-        public static final int action_refresh=0x7f080014;
-        public static final int action_settings=0x7f080013;
+        public static final int action_add=0x7f080023;
+        public static final int action_refresh=0x7f080025;
+        public static final int action_settings=0x7f080024;
         public static final int activity_edit=0x7f080000;
-        public static final int booklist=0x7f08000b;
-        public static final int edit_action_cancel=0x7f08000c;
-        public static final int edit_action_ok=0x7f08000d;
-        public static final int edit_date=0x7f080001;
-        public static final int edit_price=0x7f080003;
-        public static final int edit_time=0x7f080002;
-        public static final int items_price=0x7f08000f;
-        public static final int items_purpose=0x7f080010;
-        public static final int items_time=0x7f080011;
-        public static final int items_way_img=0x7f08000e;
-        public static final int list_purpose=0x7f080004;
-        public static final int radioButton_alipay=0x7f080008;
-        public static final int radioButton_borrow=0x7f080009;
-        public static final int radioButton_card=0x7f080007;
-        public static final int radioButton_cash=0x7f080006;
-        public static final int sumlabel=0x7f08000a;
-        public static final int ways_group=0x7f080005;
+        public static final int booklist=0x7f08000c;
+        public static final int button_del=0x7f080012;
+        public static final int dialog_edit_price=0x7f08000d;
+        public static final int dialog_edit_price_hint=0x7f080001;
+        public static final int edit_action_cancel=0x7f08001d;
+        public static final int edit_action_ok=0x7f08001e;
+        public static final int edit_date=0x7f080002;
+        public static final int edit_price=0x7f080004;
+        public static final int edit_time=0x7f080003;
+        public static final int items_price=0x7f080020;
+        public static final int items_purpose=0x7f080021;
+        public static final int items_time=0x7f080022;
+        public static final int items_way_img=0x7f08001f;
+        public static final int list_purpose=0x7f080005;
+        public static final int number0=0x7f08001c;
+        public static final int number00=0x7f080017;
+        public static final int number1=0x7f080019;
+        public static final int number2=0x7f08001a;
+        public static final int number3=0x7f08001b;
+        public static final int number4=0x7f080014;
+        public static final int number5=0x7f080015;
+        public static final int number6=0x7f080016;
+        public static final int number7=0x7f08000f;
+        public static final int number8=0x7f080010;
+        public static final int number9=0x7f080011;
+        public static final int radioButton_alipay=0x7f080009;
+        public static final int radioButton_borrow=0x7f08000a;
+        public static final int radioButton_card=0x7f080008;
+        public static final int radioButton_cash=0x7f080007;
+        public static final int sumlabel=0x7f08000b;
+        public static final int tableRow1=0x7f08000e;
+        public static final int tableRow2=0x7f080013;
+        public static final int tableRow3=0x7f080018;
+        public static final int ways_group=0x7f080006;
     }
     public static final class layout {
         public static final int activity_edit=0x7f030000;
         public static final int activity_main=0x7f030001;
-        public static final int edit_custom_actionbar=0x7f030002;
-        public static final int items=0x7f030003;
+        public static final int dialog_add_price=0x7f030002;
+        public static final int edit_custom_actionbar=0x7f030003;
+        public static final int items=0x7f030004;
     }
     public static final class menu {
         public static final int main=0x7f070000;
@@ -69,6 +87,7 @@ public final class R {
         public static final int card=0x7f05000e;
         public static final int cash=0x7f05000d;
         public static final int desc=0x7f050005;
+        public static final int dialog_add_price_title=0x7f050011;
         public static final int hello_world=0x7f050001;
         public static final int items_price=0x7f050008;
         public static final int items_purpose=0x7f050007;
--- src/com/haoli/haoli/EditActivity.java
+++ src/com/haoli/haoli/EditActivity.java
@@ -1,17 +1,22 @@
 package com.haoli.haoli;
 
+import java.text.DecimalFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import android.annotation.SuppressLint;
 import android.app.ActionBar;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.View.OnLongClickListener;
 //import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.RadioButton;
@@ -104,10 +109,25 @@ public class EditActivity extends Activity {
 			}
 		});
 		
+		edit_price.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View v) {
+				show_dialog_add_price(0.00);
+			}
+		});
+		
+		//((TextView) findViewById(R.id.dialog_edit_price_hint)).setOnClickListener(new OnClickListener(){
+		//	@Override
+		//	public void onClick(View v) {
+			//	show_dialog_add_price(0.00);
+			//}
+		//});
+		
 		if(mode == 1) {
 			edit_date.setText(new SimpleDateFormat(format_date_toread).format(new Date()));
 			edit_time.setText(new SimpleDateFormat(format_time_toread).format(new Date()));
 			edit_price.setText("0.00");
+			show_dialog_add_price(0.00);
 			//TODO:initialize ListView
 			//TODO:Price setting dialog
 			
@@ -143,7 +163,7 @@ public class EditActivity extends Activity {
 		if(ischecked){
 			Intent result = new Intent();
 			result.putExtra("time",new SimpleDateFormat(format_date_tostore+format_time_tostore).format(new Date()));//TODO:changetime
-			result.putExtra("price", "12.00");
+			result.putExtra("price", edit_price.getText().toString());
 			result.putExtra("purpose", "undef");
 			result.putExtra("way", chosenway);
 			setResult(RESULT_OK,result);
@@ -152,5 +172,152 @@ public class EditActivity extends Activity {
 		else
 			 Toast.makeText(this, "Unable to finish", Toast.LENGTH_SHORT).show();
 			
+	}
+	
+	public void show_dialog_add_price(double Default) {
+		LayoutInflater inflater = this.getLayoutInflater();
+		final View view = (View) inflater.inflate(R.layout.dialog_add_price, null);
+		final TextView dialog_edit_price = (TextView) view.findViewById(R.id.dialog_edit_price);
+		((TextView) view.findViewById(R.id.number1)).setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				dialog_add_number(1,dialog_edit_price);
+			}
+		});
+		
+		((TextView) view.findViewById(R.id.number1)).setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				dialog_add_number(1,dialog_edit_price);
+			}
+		});
+		
+		((TextView) view.findViewById(R.id.number2)).setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				dialog_add_number(2,dialog_edit_price);
+			}
+		});
+		
+		((TextView) view.findViewById(R.id.number3)).setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				dialog_add_number(3,dialog_edit_price);
+			}
+		});
+		
+		((TextView) view.findViewById(R.id.number4)).setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				dialog_add_number(4,dialog_edit_price);
+			}
+		});
+		
+		((TextView) view.findViewById(R.id.number5)).setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				dialog_add_number(5,dialog_edit_price);
+			}
+		});
+		
+		((TextView) view.findViewById(R.id.number6)).setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				dialog_add_number(6,dialog_edit_price);
+			}
+		});
+		
+		((TextView) view.findViewById(R.id.number7)).setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				dialog_add_number(7,dialog_edit_price);
+			}
+		});
+		
+		((TextView) view.findViewById(R.id.number8)).setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				dialog_add_number(8,dialog_edit_price);
+			}
+		});
+		
+		((TextView) view.findViewById(R.id.number9)).setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				dialog_add_number(9,dialog_edit_price);
+			}
+		});
+		
+		((TextView) view.findViewById(R.id.number0)).setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				dialog_add_number(0,dialog_edit_price);
+			}
+		});
+		
+		((TextView) view.findViewById(R.id.number00)).setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				dialog_add_number(10,dialog_edit_price);
+			}
+		});
+		
+		((TextView) view.findViewById(R.id.button_del)).setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				dialog_add_number(-1,dialog_edit_price);
+			}
+		});
+		
+		((TextView) view.findViewById(R.id.button_del)).setOnLongClickListener(new OnLongClickListener(){
+
+			@Override
+			public boolean onLongClick(View arg0) {
+				dialog_add_number(-2,dialog_edit_price);
+				return true;
+			}
+		});
+		
+		//dialog_edit_price.setText(new DecimalFormat("0.00").format(Default));
+		dialog_edit_price.setText(edit_price.getText());
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		builder.setTitle(R.string.dialog_add_price_title);
+		builder.setView(view);
+		builder.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+							@Override
+								public void onClick(DialogInterface dialog, int which) {
+								TextView dialog_edit_price = (TextView) view.findViewById(R.id.dialog_edit_price);
+								changeEditPrice(dialog_edit_price.getText().toString());
+							}
+						});
+		builder.setNegativeButton(android.R.string.cancel,null);
+		AlertDialog alert = builder.create();
+		alert.show();
+	}
+	
+	public void changeEditPrice(String text) {
+		edit_price.setText(text);
+	}
+	
+	public void dialog_add_number (int num, TextView tochange) {
+		double before;
+		try {
+			before = new DecimalFormat("0.00").parse(tochange.getText().toString()).doubleValue();
+		} catch (ParseException e) {
+			before = 0.00;
+		}
+		if(before < 99999.99) {
+		if(num != 10 && num != -1)
+			before = before * 10 + num*0.01;
+		else if(num == 10)
+			before *= 100;
+		}
+		if(num == -1) {
+			before = (double) ((long)(before*10)/100.0);
+		}
+		if(num == -2) {
+			before = 0.00;
+		}
+		tochange.setText( new DecimalFormat("0.00").format(before));
 	}
 }
